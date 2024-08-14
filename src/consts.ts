762d79abe337e78fccfb7990bb9a429bbab01ce5
Ryota-Kobayashi <36566083+Ryota-Kobayashi@users.noreply.github.com>
2024-08-14 21:49:35 +0900

    Meta情報を追加

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "小林 良太";
+export const SITE_DESCRIPTION =
+  "福岡と大阪を中心にコンサルティング事業とWeb制作事業を行う小林 良太の個人サイト兼Blogです。Blogでは仕事で得た知見を中心に、仕事用に読んだ本の読書メモなどおそらく誰かのためになる情報を発信していきます。";
