c2aa9285ecaa3f7c5808570bb5b460434b01db79
Ryota-Kobayashi <36566083+Ryota-Kobayashi@users.noreply.github.com>
2024-08-15 13:19:06 +0900

    Blogのスキーマを調整

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,10 +5,8 @@ const blog = defineCollection({
   // Type-check frontmatter using a schema
   schema: z.object({
     title: z.string(),
-    description: z.string(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
     tags: z.array(z.string()).optional(),
   }),
