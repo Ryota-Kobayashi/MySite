3f5f1a3557a5c71e931f1b9abb395870014bfd6d
Ryota-Kobayashi <36566083+Ryota-Kobayashi@users.noreply.github.com>
2024-08-16 09:00:37 +0900

    GA4の設定を見直し

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
-// https://astro.build/config
+
 export default defineConfig({
   site: "https://r-kobayashi.com",
   integrations: [
