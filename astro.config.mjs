6f5ab51bfdb693257a8e7d9b3b5a848481e3fb81
Ryota-Kobayashi <36566083+Ryota-Kobayashi@users.noreply.github.com>
2024-08-16 09:42:13 +0900

    GA4を削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import partytown from "@astrojs/partytown";
 
 export default defineConfig({
   site: "https://r-kobayashi.com",
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [mdx(), sitemap(), tailwind()],
 });
