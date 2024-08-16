03c579c61ba73ea08f60a089e80ec35c356394f0
Ryota-Kobayashi <36566083+Ryota-Kobayashi@users.noreply.github.com>
2024-08-16 21:12:14 +0900

    robots.txtの拡張

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://r-kobayashi.com",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), robotsTxt()]
 });
